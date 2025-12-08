281a840d7021fea8749a895ae39bd088a176eccf
Marc Lewis <marc.lewis@justeattakeaway.com>
2025-12-08 20:25:55 +0000

    wip: attemp day 3 part 2

--- 2025/Day 3/main.go
+++ 2025/Day 3/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strings"
 )
 
-func main() {
-	// file, err := os.ReadFile("example.txt")
-	file, err := os.ReadFile("input.txt")
+func partOne() int {
+	fmt.Println("Part 1")
+	file, err := os.ReadFile("example.txt")
+	// file, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -27,7 +29,60 @@ func main() {
 			}
 		}
 		// add the product
+		// fmt.Println(line)
+		// fmt.Println(nums)
 		count += (nums[0] * 10) + nums[1]
 	}
-	fmt.Printf("count: %d\n", count)
+
+	return count
 }
+
+func partTwo(numCellsToTurnOn int) int {
+	fmt.Println("Part 2")
+	// file, err := os.ReadFile("input.txt")
+	file, err := os.ReadFile("example.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	count := 0
+	banks := strings.Split(strings.TrimSpace(string(file)), "\n")
+	for _, bank := range banks {
+		joltages := make([]int, numCellsToTurnOn)
+		bankLength := len(bank)
+		for i := 0; i < bankLength; i++ {
+			cell := int(bank[i] - '0')
+			remainingCells := bankLength - i
+			pointerStart := 0
+			if (remainingCells >= numCellsToTurnOn) {
+				pointerStart = 0
+			} else {
+				pointerStart = len(joltages) - remainingCells
+			}
+			for j := pointerStart; j < len(joltages); j++ {
+				if (cell > joltages[j]) {
+					joltages[j] = cell
+					for k := j + 1; k < len(joltages); k++ {
+						joltages[k] = int(bank[i + k] - '0')
+					}
+					break
+				}
+			}
+		}
+		fmt.Println(bank)
+		fmt.Println(joltages)
+		for i, num := range joltages {
+			count += num * int(math.Pow(10, float64(numCellsToTurnOn - i - 1)))
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Printf("Part 1: %d\n", partOne())
+	fmt.Printf("Part 2: %d\n", partTwo(3))
+}
+
+// ABCDEFGHI
+// X
+// 0000
